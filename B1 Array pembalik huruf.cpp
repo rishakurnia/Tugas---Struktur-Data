5d5a931ee86fd83b4d72445b3e633eb391a0d035
rishakurnia <72644266+rishakurnia@users.noreply.github.com>
2021-03-08 20:49:58 +0700

    Add files via upload

---
+++ B1 Array pembalik huruf.cpp
@@ -0,0 +1,25 @@
+// Praktikum II struktur data //
+// Risha Kurnia Fitri //
+// 20051397009 //
+// 2020A //
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "PROGRAM ARRAY MEMBALIK HURUF" << endl;
+	cout << "============================" << endl;
+	char a[6] = {'p', 'e', 'n', 's', 'i', 't'};
+	int i, j;
+	
+	cout << "--Kata sebelum dibalik : --"<< endl;
+	for (i=0; i<6; i++){
+		cout << a [i];	
+	}
+	cout << "\n\n";
+	cout << "--kata setelah dibalik : --"<< endl;
+	for (j=6; j>=0; j--){
+		cout << a [j];
+	}
+	return 0;
+}
